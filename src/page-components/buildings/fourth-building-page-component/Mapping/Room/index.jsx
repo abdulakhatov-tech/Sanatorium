69165361f4dc1e73f1d3387ca647bf04a582fa7a
Islomjon6975 <islomabdulakhatov6975@gmail.com>
2023-09-02 06:55:49 +0500

    Fourth building page component

---
+++ src/page-components/buildings/fourth-building-page-component/Mapping/Room/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { LoadingOutlined } from '@ant-design/icons';
+
+import { Tooltip } from '../../../../../tools';
+import { Room } from '../../../../../tools/styles';
+import { setUserID } from '../../../../../store/userSlice';
+import useQueryHandler from '../../../../../hooks/useQuery';
+import { leftDays } from '../../../../../helpers/date.helper';
+import { setUserModalVisibility } from '../../../../../store/modalSlice';
+
+const RoomComponent = ({ value: { userID } }) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+
+  const { isLoading, data } = useQueryHandler({
+    queryKey: `user/${userID}`,
+    queryLink: `/accomodation/4/user?_id=${userID}`,
+  });
+
+  // set userID
+  useEffect(() => {
+    dispatch(setUserID({ userID: userID }));
+  }, [userID, dispatch]);
+
+  return (
+    <Tooltip title={t('tooltip.busy_room')} color={'red'}>
+      <Room
+        color="red"
+        onClick={() => !isLoading && dispatch(setUserModalVisibility())}
+      >
+        {isLoading ? <LoadingOutlined /> : leftDays(data.endDate)}
+      </Room>
+    </Tooltip>
+  );
+};
+
+export default RoomComponent;
--- src/page-components/buildings/fourth-building-page-component/index.jsx
+++ src/page-components/buildings/fourth-building-page-component/index.jsx
@@ -1,5 +1,27 @@
+import { useTranslation } from 'react-i18next';
+
+import Mapping from './Mapping';
+import { UserModal } from '../../../components';
+import { ArrowBack, Loading } from '../../../tools';
+import useQueryHandler from '../../../hooks/useQuery';
+import { CenteredWrapper } from '../../../tools/styles';
+
 const FourthBuildingPageComponent = () => {
-  return <div>FourthBuildingPageComponent</div>;
+  const { t } = useTranslation();
+
+  const { isLoading } = useQueryHandler({
+    queryKey: 'accommodation/4',
+    queryLink: '/accomodation/4/room',
+  });
+
+  return (
+    <CenteredWrapper>
+      <ArrowBack translation={`4 ${t('building_control_page.building')}`} />
+      <UserModal />
+
+      {isLoading ? <Loading /> : <Mapping />}
+    </CenteredWrapper>
+  );
 };
 
 export default FourthBuildingPageComponent;
